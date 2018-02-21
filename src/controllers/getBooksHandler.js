d95d8d925d2cb52f73a57152afc958efb22b893e
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-21 13:35:33 +0530

    Modify route handlers to give required attributes

--- src/controllers/getBooksHandler.js
+++ src/controllers/getBooksHandler.js
@@ -2,7 +2,9 @@ const Models = require('../../models');
 
 const handler = (request, response) => {
   console.log('GET /books');
-  Models.books.findAll().then((allBooks) => {
+  Models.books.findAll({
+    attributes: ['author', 'bookID', 'name', 'rating'],
+  }).then((allBooks) => {
     response(allBooks).code(200);
   });
 };
--- src/controllers/getLikesHandler.js
+++ src/controllers/getLikesHandler.js
@@ -1,7 +1,9 @@
 const Models = require('../../models');
 
 const handler = (request, response) => {
-  Models.likes.findAll().then((allLikes) => {
+  Models.likes.findAll({
+    attributes: ['bookID', 'likeStatus'],
+  }).then((allLikes) => {
     response(allLikes).code(200);
   });
 };
